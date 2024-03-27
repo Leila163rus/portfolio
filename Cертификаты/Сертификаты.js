0b5a3d68a3f578c4b7a2fab2a891a6d2e4ad09d2
Пользователь <lilu163@mail.ru>
2024-03-27 22:29:10 +0400

    Added project

---
+++ Cертификаты/Сертификаты.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const image = document.querySelectorAll('img');
+
+image.forEach((e) => {
+    e.addEventListener('mouseover', () => {
+        e.style.transform = 'scale(1.2)';
+        e.style.boxShadow = '0 25px 23px rgba(0, 0, 0, 0.50)';
+        e.style.transition = 'all 0.5s ease-out';
+    });
+    })
+
+image.forEach((e) => {
+    e.addEventListener('mouseout', () => {
+        e.style.transform = 'scale(1)';
+        e.style.boxShadow = 'none';
+        e.style.transition = 'all 0.7s ease-out';
+    })
+});
+
---
+++ Проекты/Проекты.js
@@ -0,0 +1,36 @@
+import {proekt} from './data.js'
+
+'use strict'
+
+const dscrip = document.getElementById('clock');
+const pred = document.getElementById('prediction');
+const weath = document.getElementById('weather')
+
+putDscrip();
+
+putPred();
+
+getWeath();
+
+function putDscrip() {
+    if (dscrip.textContent === '') {
+        dscrip.textContent = proekt.clock;
+    }
+}
+
+function putPred() {
+    if (pred.textContent === '') {
+        pred.textContent = proekt.prediction;
+    }
+}
+
+function getWeath() {
+    if (weath.textContent === '') {
+        weath.textContent = proekt.weather;
+        console.log(weath.textContent);
+    }
+}
+
+
+
+
