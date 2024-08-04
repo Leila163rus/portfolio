fc8252aea4495359f1b412244f98c6cc54024248
Пользователь <lilu163@mail.ru>
2024-08-05 02:07:52 +0400

    adaptation

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-'use strict';
-
-
+/*function setZoom() {
+  if (window.matchMedia('(min-width: 360px) and (max-width: 1280px)').matches) {
+  document.body.style.zoom = "24%";
+  } else {
+  document.body.style.zoom = "100%";
+  }
+}
+setZoom();
+window.addEventListener('resize', setZoom);*/
